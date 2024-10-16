bcfb82ea7998aafea0c09099e5126a6102cfdcce
S0k1l <martynuky16@gmail.com>
2024-10-16 20:55:51 +0300

    Added roles adn seed

---
+++ ClinicAPI/ClinicAPI/Data/Seed.cs
@@ -0,0 +1,26 @@
+﻿using ClinicAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Diagnostics;
+using System.Net;
+
+namespace ClinicAPI.Data
+{
+    public class Seed
+    {
+        public static async Task SeedData(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+
+                //Roles
+                var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                if (!await roleManager.RoleExistsAsync(UserRoles.Doctor))
+                    await roleManager.CreateAsync(new IdentityRole(UserRoles.Doctor));
+                if (!await roleManager.RoleExistsAsync(UserRoles.Patient))
+                    await roleManager.CreateAsync(new IdentityRole(UserRoles.Patient));
+
+            }
+        }
+    }
+}
---
+++ ClinicAPI/ClinicAPI/Data/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace ClinicAPI.Data
+{
+    public static class UserRoles
+    {
+        public const string Doctor = "doctor";
+        public const string Patient = "patient";
+    }
+}
