e35e5b879604b558ca563672766697b4a575e08e
S0k1l <martynuky16@gmail.com>
2024-10-26 17:23:51 +0300

    Updated AuthService

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -2,32 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { LoginRequest } from '../interfaces/login-request';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
 import { jwtDecode } from 'jwt-decode';
 import { AuthResponse } from '../interfaces/auth-response';
 import { RegisterRequest } from '../interfaces/register-request';
+import { map } from 'rxjs/internal/operators/map';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  apiUrl:string = environment.apiUrl;
+  apiUrl: string = environment.apiUrl;
   private userKey = 'user';
 
   constructor(private http: HttpClient) {}
 
-  login(data:LoginRequest): Observable<AuthResponse>{
+  login(data: LoginRequest): Observable<AuthResponse> {
     return this.http
-    .post<AuthResponse>(`${this.apiUrl}account/login`, data)
-    .pipe(
-      map((response)=> {
-        if(response.isSuccess){
-          localStorage.setItem(this.userKey,JSON.stringify(response))
-        }
-        return response
-      })
-    );
+      .post<AuthResponse>(`${this.apiUrl}account/login`, data)
+      .pipe(
+        map((response) => {
+          if (response.isSuccess) {
+            localStorage.setItem(this.userKey, JSON.stringify(response));
+          }
+          return response;
+        })
+      );
   }
 
   register(data: RegisterRequest): Observable<AuthResponse> {
@@ -36,40 +36,46 @@ export class AuthService {
 
   logout(): void {
     localStorage.removeItem(this.userKey);
-  };
+  }
 
   isLoggedIn(): boolean {
     const token = this.getToken();
     if (!token) return false;
     return true;
-  };
+  }
 
   getToken(): string | null {
     const user = localStorage.getItem(this.userKey);
-    if(!user) return null;
-    const userDetail:AuthResponse = JSON.parse(user);
+    if (!user) return null;
+    const userDetail: AuthResponse = JSON.parse(user);
     return userDetail.token;
   }
 
   refreshToken(data: {
-    email: string,
+    email: string;
     token: string;
     refreshToken: string;
   }): Observable<AuthResponse> {
-    return this.http.post<AuthResponse>(`${this.apiUrl}account/refresh-token`, data);
+    return this.http.post<AuthResponse>(
+      `${this.apiUrl}account/refresh-token`,
+      data
+    );
   }
 
   getUserDetail() {
     const token = this.getToken();
-    if(!token) return null;
-    const decodedToken : any = jwtDecode(token);
+    if (!token) return null;
+
+    const decodedToken: any = jwtDecode(token);
+
     const userDetail = {
-      id: decodedToken.namid,
+      id: decodedToken.nameid,
       fullName: decodedToken.name,
       email: decodedToken.email,
-      roles: decodedToken.role || [],
-    }
-    return userDetail
+      roles: decodedToken.role ? [decodedToken.role] : [],
+    };
+
+    return userDetail;
   }
 
   getRefreshToken(): string | null {
@@ -77,5 +83,5 @@ export class AuthService {
     if (!user) return null;
     const userDetail: AuthResponse = JSON.parse(user);
     return userDetail.refreshToken;
-  };
+  }
 }
